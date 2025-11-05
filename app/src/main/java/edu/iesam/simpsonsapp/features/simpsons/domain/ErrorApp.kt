f0070d309bbc92e7edd7c7a557d8d7b0db35429d
Xumi22 <davidgallego20@gmail.com>
2025-11-05 23:06:01 +0100

    "feat(data): implementada capa Data con Retrofit, Gson y corutinas"

--- app/src/main/java/edu/iesam/simpsonsapp/features/simpsons/domain/ErrorApp.kt
+++ app/src/main/java/edu/iesam/simpsonsapp/features/simpsons/domain/ErrorApp.kt
@@ -1,7 +1,7 @@
 package edu.iesam.simpsonsapp.features.simpsons.domain
 
 //Sealed es para sellar clase y que solo tengo dos atributos
-sealed class ErrorApp: Throwable() {
+sealed class ErrorApp : Throwable() {
     //object es para no crear mas instancias de los atributos
     object NetworkError : ErrorApp()
     object ServerError : ErrorApp()
